106b9c604d06a294711d6ea6ae168ac6482e8114
Sebastian Klaus <sebastian@lightapps.de>
2026-01-06 11:45:43 +0100

    Entferne nicht verwendete Konfigurationseinstellungen

--- doctum.php
+++ doctum.php
@@ -14,17 +14,5 @@ return new Doctum(
         'build_dir' => __DIR__ . '/build/docs',
         'cache_dir' => __DIR__ . '/build/.doctum_cache',
         'theme' => 'default',
-        'include_parent_classes' => true,
-        'sort_class_properties' => true,
-        'sort_class_constants' => true,
-        'sort_class_methods' => false,
-        'sort_class_traits' => false,
-        'base_url' => 'https://caspahouzer.github.io/lemonsqueezy-api-client/',
-        'source_url' => 'https://github.com/caspahouzer/lemonsqueezy-php-client/blob/main',
-        'source_dir' => '/src',
-        'parse_private' => false,
-        'parse_protected' => true,
-        'default_opened' => 'classes',
-        'download' => true,
     ]
 );
